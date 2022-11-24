f785763fb282ae54fdc81199fd79bbe578dd335b
00012938 <abdulazizdusbabaev@gmail.com>
2022-11-24 11:49:25 +0500

    new section for webpage

--- js/accessuars.js
+++ js/accessuars.js
@@ -3,3 +3,66 @@ function slide(){
     document.getElementById("my-img").style.clipPath = "polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)";
     console.log("polygon(0 0," + slideValue + "% 0," + slideValue + "% 100%, 0 100%)");
   }
+
+
+/*moving the products section with both radio button and ordinary button that defines the group*/
+function margin_move1(){
+  let object = document.getElementById("object-of-move");
+  const keyboard_section = document.getElementById("keyboard")
+  const mouse = document.getElementById("mouse")
+  const headphone = document.getElementById("headphone")
+  const others = document.getElementById("others")
+  object.style.marginLeft = "2%";
+  keyboard_section.classList.toggle("active")
+  mouse.classList.remove("active")
+  headphone.classList.remove("active")
+  others.classList.remove("active")
+}
+function margin_move2(){
+  let object = document.getElementById("object-of-move");
+  const keyboard_section = document.getElementById("keyboard")
+  const mouse = document.getElementById("mouse")
+  const headphone = document.getElementById("headphone")
+  const others = document.getElementById("others")
+  object.style.marginLeft = "-100%";
+  keyboard_section.classList.remove("active")
+  mouse.classList.add("active")
+  headphone.classList.remove("active")
+  others.classList.remove("active")
+}
+function margin_move3(){
+  let object = document.getElementById("object-of-move");
+  const keyboard_section = document.getElementById("keyboard")
+  const mouse = document.getElementById("mouse")
+  const headphone = document.getElementById("headphone")
+  const others = document.getElementById("others")
+  object.style.marginLeft = "-200%";
+  keyboard_section.classList.remove("active")
+  mouse.classList.remove("active")
+  headphone.classList.toggle("active")
+  others.classList.remove("active")
+}
+function margin_move4(){
+  let object = document.getElementById("object-of-move");
+  const keyboard_section = document.getElementById("keyboard")
+  const mouse = document.getElementById("mouse")
+  const headphone = document.getElementById("headphone")
+  const others = document.getElementById("others")
+  object.style.marginLeft = "-300%";
+  keyboard_section.classList.remove("active")
+  mouse.classList.remove("active")
+  headphone.classList.remove("active")
+  others.classList.add("active")
+}
+function margin_move5(){
+  let object = document.getElementById("object-of-move");
+  const keyboard_section = document.getElementById("keyboard")
+  const mouse = document.getElementById("mouse")
+  const headphone = document.getElementById("headphone")
+  const others = document.getElementById("others")
+  object.style.marginLeft = "-400%";
+  keyboard_section.classList.remove("active")
+  mouse.classList.remove("active")
+  headphone.classList.remove("active")
+  others.classList.add("active")
+}
